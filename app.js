dc9b851e22b7a3a1476b4a99b1fb3b8218617132
Pramod <pramodraina047@gmail.com>
2022-10-31 05:58:51 +0000

    Initialized DB and Server

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const express = require("express");
 
+const app = express();
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const path = require("path");
+const dbPath = path.join(__dirname, "cricketMatchDetails.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000/");
+    });
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
